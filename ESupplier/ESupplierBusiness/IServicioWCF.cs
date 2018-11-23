40db15b4a0a24709b4a1f490d587aa811256d12d
jairocombitareyes <36902427+jairocombitareyes@users.noreply.github.com>
2018-11-23 17:21:40 -0500

    WSDL

---
+++ ESupplier/ESupplierBusiness/IServicioWCF.cs
@@ -0,0 +1,27 @@
+﻿using ESupplierBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web.Http;
+
+namespace ESupplierBusiness
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioWCF" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioWCF
+    {
+        [OperationContract]
+        void DoWork();
+
+
+        
+
+        [OperationContract]
+        void  PostWINNERS(WINNERS wINNERS);
+
+
+    }
+}
---
+++ ESupplier/ESupplierBusiness/ServicioWCF.svc.cs
@@ -0,0 +1,31 @@
+﻿using ESupplierBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ESupplierBusiness
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioWCF" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioWCF.svc o ServicioWCF.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioWCF : IServicioWCF
+    {
+        
+        private Controllers.WINNERSController controlador = new Controllers.WINNERSController();
+
+        public void DoWork()
+        {
+        }
+
+        public void PostWINNERS(WINNERS wINNERS) {
+            controlador.PostWINNERS(wINNERS);
+        }
+
+        
+
+
+       
+    }
+}
